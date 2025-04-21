f3ce291d9182a9a55f6822b3d307af0471dbb224
Shubh Gupta <142342135+titanShubh@users.noreply.github.com>
2025-04-21 17:46:18 +0000

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Index />} /> */}
+          <Route path="/" element={<Index />} />
           {/* <Route path="/contact" element={<Contact />} /> */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
